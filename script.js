73bf392f1dd6a8fdb0665a4a48842409d7b79285
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2025-04-02 21:07:20 +0700

    bổ sung code gen v1 và v2

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let qrcode = null;
 document
   .getElementById("generate-btn")
   .addEventListener("click", generateQRCode);
+document.getElementById("type-qr-v1").addEventListener("click", generateQRCode);
+document.getElementById("type-qr-v2").addEventListener("click", generateQRCode);
 document
   .getElementById("download-btn")
   .addEventListener("click", downloadQRCode);
@@ -12,22 +14,51 @@ function generateQRCode() {
     return;
   }
   // Xóa QR code cũ nếu có
-  const qrcodeDiv = document.getElementById("qrcode");
+  let qrcodeDiv = document.getElementById("qrcode");
   qrcodeDiv.innerHTML = "";
   // Tạo QR code mới
   let simpleText = text.replace(/(\r\n|\n|\r)/gm, "");
-  const shouldEncode = document.getElementById("encode-checkbox").checked;
-  qrcode = new QRCode(qrcodeDiv, {
-    text: shouldEncode ? btoa(simpleText) : simpleText,
-    width: 512,
-    height: 512,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H,
-  });
+  let shouldEncode = document.getElementById("encode-checkbox").checked;
+  let usingQRV1 = document.getElementById("type-qr-v1").checked;
+  let textBuild = shouldEncode ? btoa(simpleText) : simpleText;
+  if (usingQRV1) {
+    qrcode = new QRCode(qrcodeDiv, {
+      text: textBuild,
+      width: 512,
+      height: 512,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+  } else {
+    let segs = qrcodegen.QrSegment.makeSegments(textBuild);
+    qrcode = qrcodegen.QrCode.encodeSegments(segs, qrcodegen.QrCode.Ecc.LOW);
+    drawCanvas(qrcode, 9, 4, "#ffffff", "#000000", appendCanvas());
+  }
+
   // Hiện nút tải xuống
   document.getElementById("download-section").style.display = "block";
 }
+function drawCanvas(qr, scale, border, lightColor, darkColor, canvas) {
+  if (scale <= 0 || border < 0) throw new RangeError("Value out of range");
+  var width = (qr.size + border * 2) * scale;
+  canvas.width = width;
+  canvas.height = width;
+  var ctx = canvas.getContext("2d");
+  for (var y = -border; y < qr.size + border; y++) {
+    for (var x = -border; x < qr.size + border; x++) {
+      ctx.fillStyle = qr.getModule(x, y) ? darkColor : lightColor;
+      ctx.fillRect((x + border) * scale, (y + border) * scale, scale, scale);
+    }
+  }
+}
+function appendCanvas() {
+  var outputElem = document.getElementById("qrcode");
+  var result = document.createElement("canvas");
+  outputElem.appendChild(result);
+  return result;
+}
+
 function downloadQRCode() {
   if (!qrcode) {
     alert("Vui lòng tạo mã QR trước khi tải xuống!");
